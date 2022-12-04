43445a773c8324528c201e6fd95fc297d060fc27
Shirash Shrestha <shirashshrestha10@gmail.com>
2022-12-04 23:27:35 +0545

    Second From AS

--- app/src/main/java/com/project/springclient/MainActivity.java
+++ app/src/main/java/com/project/springclient/MainActivity.java
@@ -3,6 +3,20 @@ package com.project.springclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.project.springclient.model.Employee;
+import com.project.springclient.retrofit.EmployeeApi;
+import com.project.springclient.retrofit.RetrofitService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        TextInputEditText inputEditTextName = findViewById(R.id.form_textFieldName);
+        TextInputEditText inputEditTextBranch = findViewById(R.id.form_textFieldBranch);
+        TextInputEditText inputEditTextLocation = findViewById(R.id.form_textFieldLocation);
+        MaterialButton buttonSave = findViewById(R.id.form_buttonSave);
+
+        RetrofitService retrofitService = new RetrofitService();
+        EmployeeApi employeeApi = retrofitService.getRetrofit().create(EmployeeApi.class);
+
+        buttonSave.setOnClickListener(view -> {
+            String name = String.valueOf(inputEditTextName.getText().toString());
+            String branch = String.valueOf(inputEditTextBranch.getText().toString());
+            String location = String.valueOf(inputEditTextLocation.getText().toString());
+
+            Employee employee = new Employee();
+            employee.setName(name);
+            employee.setBranch(branch);
+            employee.setLocation(location);
+
+            employeeApi.save(employee)
+                    .enqueue(new Callback<Employee>() {
+                        @Override
+                        public void onResponse(Call<Employee> call, Response<Employee> response) {
+                            Toast.makeText(MainActivity.this, "Save Successful!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<Employee> call, Throwable t) {
+                            Toast.makeText(MainActivity.this, "Save Failed!!!", Toast.LENGTH_SHORT).show();
+                            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, "Error occurred", t);
+                        }
+                    });
+        });
     }
 }
---
+++ app/src/main/java/com/project/springclient/retrofit/EmployeeApi.java
@@ -0,0 +1,19 @@
+package com.project.springclient.retrofit;
+
+import com.project.springclient.model.Employee;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface EmployeeApi {
+
+    @GET("/employee/get-all")
+    Call<List<Employee>> getAllEmployees();
+
+    @POST("/employee/save")
+    Call<Employee> save(@Body Employee employee);
+}
